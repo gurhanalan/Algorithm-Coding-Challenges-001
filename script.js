2182abfde27bddc21e5641889d21129018e7adaf
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-05-15 04:35:11 +0300

    Adding 4. challenge

--- script.js
+++ script.js
@@ -94,8 +94,37 @@ function likes(names) {
         return `${names[0]}, ${names[1]} and ${length - 2} others likes this`;
     }
 }
-console.log(likes([]));
+
 // likes (["Peter"]) // must be "Peter likes this"
 // likes (["Jacob", "Alex"]) // must be "Jacob and Alex like this"
 // likes (["Max", "John", "Mark"]) // must be "Max, John and Mark like this"
 // likes (["Alex", "Jacob", "Mark", "Max"]) // must be "Alex, Jacob and 2 others like this"
+console.log(likes([]));
+
+// #######################################################################################
+// 4. You're a square! - codewars - 7kyu
+/* A square of squares
+You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+Task
+Given an integral number, determine if it's a square number:
+
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
+ */
+
+const isSquare = function (n) {
+    return Number.isInteger(Math.sqrt(n));
+};
+console.log(Number.isInteger(Math.sqrt(25)));
