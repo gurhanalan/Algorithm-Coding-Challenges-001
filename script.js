b8d934af19402343e2608a4957df3fea316b228b
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-05-26 20:23:29 +0300

    Add new challange

--- script.js
+++ script.js
@@ -291,3 +291,43 @@ function countUniqueValues2(arr) {
 
 console.log(countUniqueValues2([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
 console.log(countUniqueValues2([]));
+
+//////////////////////////////
+// 20. maxSubarraySum
+/* Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array. */
+
+/* 
+maxSubarraySum([1,2,5,2,8,1,5],2) // 10
+maxSubarraySum([1,2,5,2,8,1,5],4) // 17
+maxSubarraySum([4,2,1,6],1) // 6
+maxSubarraySum([4,2,1,6,2],4) // 13
+maxSubarraySum([],4) // null */
+
+// Brute force
+function maxSubarraySum(arr, num) {
+    let total;
+    let maxTotal = -Infinity;
+    for (let i = 0; i <= arr.length - num; i++) {
+        total = arr.slice(i, i + num).reduce((acc, el) => acc + el);
+        if (total > maxTotal) maxTotal = total;
+    }
+    return maxTotal;
+}
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2));
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4));
+
+// More efficient way
+
+function maxSubarraySum2(arr, num) {
+    let total = arr.slice(0, num).reduce((acc, el) => acc + el);
+    let maxTotal = -Infinity;
+    for (let i = 1; i <= arr.length - num; i++) {
+        total = total - arr[i - 1] + arr[i + num - 1];
+        if (total > maxTotal) maxTotal = total;
+    }
+    return maxTotal;
+}
+
+console.log(maxSubarraySum2([1, 2, 5, 2, 8, 1, 5], 2));
+console.log(maxSubarraySum2([1, 2, 5, 2, 8, 1, 5], 4));
